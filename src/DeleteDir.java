8a21636dd3a0e4b94db202f9715ad44dc9fcfd3c
LirolaJose <lirola_hose@mail.ru>
2020-03-11 11:55:55 +0300

    InPuts

--- src/DeleteDir.java
+++ src/DeleteDir.java
@@ -1,2 +1,14 @@
+import java.io.File;
+
 public class DeleteDir {
+    public static void deleteDirectory(File dir) {
+        if (dir.isDirectory()) {
+            String[] children = dir.list();
+            for (int i = 0; i < children.length; i++) {
+                File f = new File(dir, children[i]);
+                deleteDirectory(f);
+            }
+        }
+        dir.delete();
+    }
 }
