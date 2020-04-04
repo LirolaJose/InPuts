e0a32186613c2a88c1875fcc51400d41992d9b6c
LirolaJose <lirola_hose@mail.ru>
2020-04-04 16:07:05 +0300

    Создал объект в котром разделил номер на три части: +1; (код города); номер. Заменил коды городов согласно условию задания.

--- src/GzFile.java
+++ src/GzFile.java
@@ -8,15 +8,15 @@ public class GzFile {
     public static void unGZIP(final String filename) {
         int BUFFER_SIZE = 1024;
         byte[] buffer = new byte[BUFFER_SIZE];
-        String dir_to = filename.substring(0, filename.lastIndexOf("."));
+       /* String dir_to = filename.substring(0, filename.lastIndexOf("."));
         File direct = new File(dir_to);
         if(!direct.exists()){
             direct.mkdir();
         }
-        String unZipDir = direct.getAbsolutePath();
+        String unZipDir = direct.getAbsolutePath();*/
         try (GZIPInputStream gzipis = new GZIPInputStream(new FileInputStream(filename));
-             FileOutputStream fos = new FileOutputStream(unZipDir)) {
-             //FileOutputStream fos = new FileOutputStream(getDstFileName(filename))) {
+             //FileOutputStream fos = new FileOutputStream(unZipDir)) {
+             FileOutputStream fos = new FileOutputStream(getDstFileName(filename))) {
             int length = 0;
             while ((length = gzipis.read(buffer)) > 0) {
                 fos.write(buffer, 0, length);
