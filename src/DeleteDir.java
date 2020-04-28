1a84db3a13b6410d016acc81914a3a048fc20bda
LirolaJose <lirola_hose@mail.ru>
2020-04-28 22:30:24 +0300

    added delete folder after archiving

--- src/DeleteDir.java
+++ src/DeleteDir.java
@@ -4,8 +4,11 @@ public class DeleteDir {
     public static void deleteDirectory(File dir) {
         if (dir.isDirectory()) {
             String[] children = dir.list();
-            for (int i = 0; i < children.length; i++) {
-                File f = new File(dir, children[i]);
+            if(children==null){
+                return;
+            }
+            for (String child : children) {
+                File f = new File(dir, child);
                 deleteDirectory(f);
             }
         }
--- src/Test1.java
+++ src/Test1.java
@@ -8,7 +8,7 @@ public class Test1 {
     static Map<String, List<String>> phonesAndEmails = new HashMap<>(); // создание map c ключём String и значением Список Strings, имя Map phoneAndEmails, тип map - HashMap
 
 
-    public  void fetchChild(File dir) throws Exception { // метод fetchChild с параметром File dir
+    public void fetchChild(File dir) throws Exception { // метод fetchChild с параметром File dir
         if (dir.isDirectory()) { // условный оператор if с параметром если dir это Папка
             for (File item : dir.listFiles()) {
                 if (item.isDirectory()) { // если объект - это папка вывводит на экран путь и надпись "папка"
@@ -20,25 +20,26 @@ public class Test1 {
                         destName = absolutePath.substring(0, absolutePath.lastIndexOf(".zip"));
                         Unpack.unpackZip(absolutePath, destName);
                         DeleteDir.deleteDirectory(item);
-                        assert destName != null;
                         File newFolder = new File(destName);
                         fetchChild(newFolder);
                         String zipFile = destName + ".zip";
                         ZipPack.zip(destName, zipFile);
+                        DeleteDir.deleteDirectory(newFolder);
                     } else if (absolutePath.endsWith(".gz")) {
                         GzFile.unGZIP(absolutePath);
                         DeleteDir.deleteDirectory(item);
                         destName = GzFile.getDstFileName(absolutePath);
                         File textFile = new File(destName);
                         fetchChild(textFile);
-                        String gzFile = destName +".gz";
+                        String gzFile = destName + ".gz";
                         GzPack.compressGzipFile(destName, gzFile);
+                        DeleteDir.deleteDirectory(textFile);
                     }
                 } else {
                     Print.getNumberFromFile(item, phonesAndEmails); // print вызывает метод getNumberFromFile из класса Print и указываем путь файла.
                 }
             }
-        }else {
+        } else {
             Print.getNumberFromFile(dir, phonesAndEmails);
         }
     }
@@ -48,12 +49,13 @@ public class Test1 {
     }
 
     public static void main(String[] args) throws Exception { // метод main
-       File dir = new File("D://Programming//inputs//");
-        if(dir.exists()) {
+        /*File dir = new File("D://Programming//inputs//");
+        if (dir.exists()) {
             DeleteDir.deleteDirectory(dir);
         }
-        Unpack.unpackZip("D://Programming//inputs_v2.zip", "D://Programming//");
+        Unpack.unpackZip("D://Programming//inputs_v2.zip", "D://Programming//");*/
         Test1 example = new Test1(); // объект example класса Test1
+        File dir = new File("D://Programming//testPack//");
         example.fetchChild(dir);// переменная example вызывает метод fetchChild
         phonesAndEmails.forEach((phone, email) -> System.out.println(phone + ":" + email.toString())); // в Map для каждой пары ключ - значение выводим на экран: Ключ: Значение в строку
         MkTextFile.mkTxt(phonesAndEmails);
